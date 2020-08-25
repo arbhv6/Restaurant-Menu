5045bf0e94f234d49432f85a8e97675797079c6a
Aaron <aaron.birkett@yahoo.com>
2020-08-24 19:31:44 -0500

    second commit

--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -1,4 +1,29 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Calendar;
+
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
+
 public class Menu {
+    private ArrayList<MenuItem> menuList;
+    private LocalDateTime lastUpdate;
+
+    public static String getCurrentDate() {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+        LocalDateTime now = LocalDateTime.now();
+        return dtf.format(now);
+    }
+
+
+
+    public static void main(String[] args) {
+//        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+//        LocalDateTime now = LocalDateTime.now();
+//        System.out.println(dtf.format(now));
+
+        System.out.println(getCurrentDate());
+    }
 }
--- src/com/company/MenuItem.java
+++ src/com/company/MenuItem.java
@@ -1,4 +1,25 @@
 package com.company;
 
 public class MenuItem {
+    private String name;
+    private double price;
+    private String description;
+    private String category;
+    private Boolean newItem;
+
+    public MenuItem(String name, double price, String description, String category) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.category = category;
+        this.newItem = true;
+    }
+
+    public String getName() { return this.name; }
+    public double getPrice() { return this.price; }
+    public String getDescription() { return this.description; }
+    public String getCategory() { return this.category; }
+    public Boolean getNewItem() { return this.newItem; }
+
+
 }
